071b65a4f6b02e9a2b5b6d611fd68ee143ee8266
sametsahindogan <sametsahindogan@gmail.com>
2020-06-07 17:48:30 +0300

    mongodb auth connection

--- bootstrap/db.js
+++ bootstrap/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+// module.exports = function (user = 'user', pass = 'pass', host = '127.0.0.1', port = '27017', collection = 'static_server') {
+//     const database = `mongodb://${process.env.DB_USER || user}:${process.env.DB_PASS || pass}@${process.env.DB_HOST || host}:${process.env.DB_PORT || port}/${process.env.DB_NAME || collection}`;
 module.exports = function (host = '127.0.0.1', port = '27017', collection = 'static_server') {
     const database = `mongodb://${process.env.DB_HOST || host}:${process.env.DB_PORT || port}/${process.env.DB_NAME || collection}`;
     mongoose.connect(database, {
